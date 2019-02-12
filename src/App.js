70c0c208d543c6519913cbd6ed5f3d4b68025a57
Sebastian Awatramani <sebastian.awatramani+github@gmail.com>
2019-02-12 14:34:58 -0700

    React.createElelement testing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        // return (
+        //   <div className="App">
+        //     <h1> Hi, I am a react app.  Wait holy shit, in real time?</h1>
+        //   </div>
+        // );
+
+        return React.createElement('div', 'null',
+            React.createElement('h1', {className: 'App'}, "does this work now?"),
+            'Hi, I\'m a React App');
+
+    }
 }
 
 export default App;
