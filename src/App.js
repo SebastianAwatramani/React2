56de9e5af646803d9ea2d6372c8fe9fde73f678e
Sebastian Awatramani <sebastian.awatramani+github@gmail.com>
2019-02-12 15:24:33 -0700

    Working with state
    *Not avail in function componenets.  Only in classes extended from react
    *Use state with care.
    *React rerenders the dom when state changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: "Sebastian", age: 35},
+            {name: "Tina", age: 30},
+            {name: "Peter", age: 55}
+        ]
+    }
     render() {
         return (
           <div className="App">
             <h1> Hi, I am a react app.</h1>
               <p>This is really working</p>
-              <Person name = "Sebastian" age = "35"/>
-              <Person name = "Tina" age = "30" />
-              <Person name = "Ginger" age = "22">My Hobbies: Racing</Person>
+              <button>Switch Name</button>
+              <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+              <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
+              <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}>My Hobbies: Racing</Person>
           </div>
         );
 
