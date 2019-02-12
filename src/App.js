2fe1e0dec521881f527c57ec846d483819eb6aff
Sebastian Awatramani <sebastian.awatramani+github@gmail.com>
2019-02-12 14:41:54 -0700

    Back to jsx - mn

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 
 class App extends Component {
     render() {
-        // return (
-        //   <div className="App">
-        //     <h1> Hi, I am a react app.  Wait holy shit, in real time?</h1>
-        //   </div>
-        // );
+        return (
+          <div className="App">
+            <h1> Hi, I am a react app.  Wait holy shit, in real time?</h1>
+              <p>This is really working</p>
+          </div>
+        );
 
-        return React.createElement('div', 'null',
-            React.createElement('h1', {className: 'App'}, "does this work now?"),
-            'Hi, I\'m a React App');
+        // return React.createElement('div', 'null',
+        //     React.createElement('h1', {className: 'App'}, "does this work now?"),
+        //     'Hi, I\'m a React App');
 
     }
 }
