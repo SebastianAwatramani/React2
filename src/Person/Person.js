e48df3a7f13eecf2fcf91fd45cceb9f9724294f5
Sebastian Awatramani <sebastian.awatramani+github@gmail.com>
2019-02-15 14:09:33 -0700

    Styling with stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,10 +3,11 @@
 //import react
 
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             {/*Receiving click() method from App component*/}
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
